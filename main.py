8504963115fa0255b489ce1197a906feec6d0b02
TheNanoMachi <anthonyl99087@gmail.com>
2021-05-22 14:09:21 -0400

    Added .env

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import discord
+
 
 Client = discord.Client()
 
@@ -11,5 +13,9 @@ async def on_message(message):
   if message.authro == Client.user:
     return
   
-  if message.content.startswith('!hello'):
-    
+  if message.content.startswith('$hello'):
+    await message.channel.send("Hello!")
+
+Client.run()
+
+Client.run(os.getenv('TOKEN'))
