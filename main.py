786dba9142f4c77c4e1b88243748415b6eb6a864
Anthony Liu <>
2021-05-22 17:23:39 +0000

    Added some code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import discord
+
+Client = discord.Client()
+
+@Client.event
+async def on_ready():
+  print("we have logged in as {0.user}.format(Client)")
+
+@Client.event
+async def on_message(message):
+  if message.authro == Client.user:
+    return
+  
+  if message.content.startswith('!hello'):
+    
